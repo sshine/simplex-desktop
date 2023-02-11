9ef4cf1e9d236fc07d4255bc9795ba74eaebd076
Simon Shine <simon@simonshine.dk>
2023-02-11 18:16:22 +0100

    Add yew
    ```
    trunk build
    trunk serve
    ```

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,14 @@
+use yew::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
+}
+
+#[function_component(App)]
+pub fn app() -> Html {
+    html! {
+        <div>
+            <h2 class={"heading"}>{"Hello, World!"}</h2>
+        </div>
+    }
 }
