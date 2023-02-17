4d4a4af570d1b9442839fda1b8173135d4990df1
Simon Shine <simon@simonshine.dk>
2023-02-17 20:18:38 +0100

    Add: dependencies, 'hello' event binding, js glue

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,4 +1,13 @@
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::spawn_local;
+use web_sys::window;
 use yew::prelude::*;
+
+#[wasm_bindgen(module = "/public/glue.js")]
+extern "C" {
+    #[wasm_bindgen(js_name = invokeHello, catch)]
+    pub async fn hello(name: String) -> Result<JsValue, JsValue>;
+}
 
 fn main() {
     yew::Renderer::<App>::new().render();
@@ -6,9 +15,52 @@ fn main() {
 
 #[function_component(App)]
 pub fn app() -> Html {
+    let welcome = use_state_eq(|| "".to_string());
+    let name = use_state_eq(|| "World".to_string());
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
+
+    {
+        let welcome = welcome.clone();
+        use_effect_with_deps(
+            move |name| {
+                update_welcome_message(welcome, name.clone());
+                || ()
+            },
+            (*name).clone(),
+        );
+    }
+
+    let message = (*welcome).clone();
+
     html! {
         <div>
-            <h2 class={"heading"}>{"Hello, World!"}</h2>
+            <h2 class={"heading"}>{message}</h2>
+            <button {onclick}>{ "+1" }</button>
+            <p>{ *counter }</p>
         </div>
     }
+}
+
+fn update_welcome_message(welcome: UseStateHandle<String>, name: String) {
+    spawn_local(async move {
+        // This will call our glue code all the way through to the tauri
+        // back-end command and return the `Result<String, String>` as
+        // `Result<JsValue, JsValue>`.
+        match hello(name).await {
+            Ok(message) => {
+                welcome.set(message.as_string().unwrap());
+            }
+            Err(e) => {
+                let window = window().unwrap();
+                window.alert_with_message(&format!("Error: {e:?}")).unwrap();
+            }
+        }
+    });
 }
